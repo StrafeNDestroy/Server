f6ee8fb35b7ea665099661b32f09d261d1b9b19e
StrafeNDestroy <jacobjpadilla@outlook.com>
2023-05-30 04:45:58 -0500

    Fixed some regex patterns for intial testing

--- includes/Sourcefiles/http_request.cpp
+++ includes/Sourcefiles/http_request.cpp
@@ -27,13 +27,13 @@ public:
     HTTP_REQUEST(const std::string& http_request)
     {
         http_message = http_request;
-        get_request_lines();
-        print_request();
+        requestlines_to_vector();
+        requestlines_to_hashtable();
 
     }
 
     // Parsing http request line by line, saving to vector
-    void get_request_lines()
+    void requestlines_to_vector()
     {
         std::istringstream iss(http_message);
         std::string line;
@@ -45,27 +45,47 @@ public:
 
                 continue;
             }
-            if(std::regex_search(line,pattern)) // if there is a message payload
+            
+            if(std::regex_search(line,pattern))
             {
                 payload_flag = 1;
-                requestLines.push_back(line);
+                std::cout << "Move the payload" << std::endl;
             }
 
             else // adding line to vector 
             {
-            std::cout << line << std::endl;
+            // std::cout << line << std::endl;
             requestLines.push_back(line);
             }
-        }    
+        }
+        http_messgae_first_line = requestLines[0];
+        requestLines.erase(requestLines.begin());
+        
+
     }
 
    void requestlines_to_hashtable()
     {
-        std::regex patter();
-        for (std::string element, requestLines)
+        // WS = WhiteSpace, hf = Header Field
+        std::regex ws_error_pattern(R"(([a-zA-Z]+\s+:))");
+        std::regex hf_pattern(R"(([a-zA-Z]+:)(\s*[a-zA-Z]+)\s*)");
+        
+        for(std::string element : requestLines)
         {
+            if (std::regex_search(element,ws_error_pattern))
+            {
+                // Send 400 (Bad Request), Stop adding to hash and send 
+                std::cout << "Follow Protocol Sir" << std::endl;
+            }
+            else
+            {
+                std::smatch matches;
+                std::regex_search(element,matches,hf_pattern);
+                std::cout <<"Key: " << matches[0] << " Value: " << matches[1] << std::endl;
 
-        }
+                          
+            }
+        }    
     }
 
 
@@ -78,11 +98,11 @@ public:
         std::cout << "Request lines:" << std::endl;
         for (const std::string& line : requestLines)
         {
-            std::cout << line << std::endl;
+            // std::cout << line << std::endl;
         }
         if(payload_flag)
         {
-            std::cout << "THERE IS A MESSAGE" << std::endl;
+            // std::cout << "THERE IS A MESSAGE" << std::endl;
         }
     }
 
@@ -91,7 +111,7 @@ public:
 
 int main()
 {
-    std::string http_request = "GET /index.html HTTP/1.1\r\nHost: example.com\r\nUser-Agent: Mozilla/5.0\r\nContent-Length: 220\r\n\r\nThis is the body stuff";
+    std::string http_request = "GET /index.html HTTP/1.1\r\nHost : example.com\r\nUserAgent: Mozilla/5.0\r\nContent-Length: 220\r\n\r\nThis is the body stuff";
     HTTP_REQUEST request(http_request);
 
     return 0;
